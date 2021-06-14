da4393a03b10c7ee680afbbfdc681cd586eadfc7
AlexnderKukachev <82620218+AlexnderKukachev@users.noreply.github.com>
2021-06-14 19:39:47 +0300

    'finishedfunc'

--- DjangoTestTask/apps/TestTask/urls.py
+++ DjangoTestTask/apps/TestTask/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'TestTask'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('d/<path:path>/', views.d, name='d'),
     path('add_pic', views.add_pic, name='add_pic'),
     path('<int:pic_id>/', views.exact_pic, name='exact_pic'),
 ]
